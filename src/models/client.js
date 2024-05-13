19df04116b449d60615d3d6b738cca0c7166df5d
Florent ATCHEAKOU <atcheakouf@gmail.com>
2024-05-13 11:18:29 +0200

    Model Client

---
+++ src/models/client.js
@@ -0,0 +1,107 @@
+const db = require('../config/db.config');
+
+const Client = function(client) {
+    this.nom = client.nom;
+    this.prenom = client.prenom;
+    this.dateNaissance = client.dateNaissance;
+    this.adresse = client.adresse;
+    this.email = client.email;
+    this.motDePasse = client.motDePasse;
+};
+
+Client.create = (newClient, result) => {
+    db.query("INSERT INTO clients SET ?", newClient, (err, res) => {
+        if(err) {
+            console.log("erreur: ", err);
+            result(err, null);
+            return;
+        }
+        console.log("client créé : ", { id: res.insertId, ...newClient });
+        result(null, { id: res.insertId, ...newClient });
+    });
+};
+
+Client.findById = (clientId, result) => {
+    db.query(`SELECT * FROM clients WHERE id = ${clientId}`, (err, res) => {
+        if(err) {
+            console.log("erreur: ", err);
+            result(err, null);
+            return;
+        }
+        if(res.length) {
+            console.log("client trouvé : ", res[0]);
+            result(null, res[0]);
+            return;
+        }
+        result({ kind: "not_found" }, null);
+    });
+};
+
+Client.getAll = result => {
+    db.query("SELECT * FROM clients", (err, res) => {
+        if(err) {
+            console.log("erreur: ", err);
+            result(null, err);
+            return;
+        }
+        console.log("clients: ", res);
+        result(null, res);
+    });
+};
+
+Client.updateById = (id, client, result) => {
+    db.query(
+        "UPDATE clients SET nom = ?, prenom = ?, dateNaissance = ?, adresse = ?, email = ?, motDePasse = ? WHERE id = ?",
+        [client.nom, client.prenom, client.dateNaissance, client.adresse, client.email, client.motDePasse, id],
+        (err, res) => {
+            if(err) {
+                console.log("erreur: ", err);
+                result(null, err);
+                return;
+            }
+            if(res.affectedRows == 0) {
+                // Utilisateur avec cet id n'a pas été trouvé
+                result({ kind: "not_found" }, null);
+                return;
+            }
+            console.log("client mis à jour : ", { id: id, ...client });
+            result(null, { id: id, ...client });
+        }
+    );
+};
+
+Client.findByEmail = (email, result) => {
+    db.query(`SELECT * FROM clients WHERE email = '${email}'`, (err, res) => {
+        if (err) {
+            console.log("erreur: ", err);
+            result(err, null);
+            return;
+        }
+        if (res.length) {
+            console.log("client trouvé : ", res[0]);
+            result(null, res[0]);
+            return;
+        }
+        result({ kind: "not_found" }, null);
+    });
+};
+
+
+Client.remove = (id, result) => {
+    db.query("DELETE FROM clients WHERE id = ?", id, (err, res) => {
+        if(err) {
+            console.log("erreur: ", err);
+            result(null, err);
+            return;
+        }
+        if(res.affectedRows == 0) {
+            // Utilisateur avec cet id n'a pas été trouvé
+            result({ kind: "not_found" }, null);
+            return;
+        }
+        console.log("client supprimé avec l'id : ", id);
+        result(null, res);
+    });
+};
+
+module.exports = Client;
