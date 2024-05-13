c23a53d63d712aa964b19cf80efa917de793802e
Florent ATCHEAKOU <atcheakouf@gmail.com>
2024-05-13 19:27:51 +0200

    Test de LOGIN  Reuissi

--- src/tests/clientsController.test.js
+++ src/tests/clientsController.test.js
@@ -5,7 +5,7 @@ describe('Client Controller', () => {
     let server;
 
     beforeEach((done) => {
-        server = app.listen(3000, done);
+        server = app.listen(3001, done);
     });
 
     afterEach((done) => {
@@ -19,13 +19,35 @@ describe('Client Controller', () => {
                 nom: 'Doe',
                 prenom: 'John',
                 date_naissance: "1989-12-31",
-                email: 'john.doe@example.com',
+                email: 'florent2@gmail.com',
                 mot_de_passe: 'securepassword',
                 adresse: '123 Main St'
             };
             const response = await request(server).post('/api/customers').send(clientData);
             expect(response.statusCode).toBe(200);
             expect(response.body).toHaveProperty('id');
+        });
+    });
+
+    // Test de connexion ou login
+    describe('POST /api/login', () => {
+        it('should authenticate a client and return a token', async () => {
+            const loginData = {
+                email: 'florent@gmail.com',
+                mot_de_passe: 'Florent228'
+            };
+            const response = await request(server).post('/api/login').send(loginData);
+            expect(response.statusCode).toBe(200);
+            expect(response.body).toHaveProperty('accessToken');
+        });
+
+        it('should return 401 if password is invalid', async () => {
+            const loginData = {
+                email: 'florent@gmail.com',
+                mot_de_passe: 'wrongpassword'
+            };
+            const response = await request(server).post('/api/login').send(loginData);
+            expect(response.statusCode).toBe(401);
         });
     });
 
