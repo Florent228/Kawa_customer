ef85a5c0a427b69f28f7625aae0bfefd8c1483a7
Florent ATCHEAKOU <atcheakouf@gmail.com>
2024-05-13 11:37:18 +0200

    code du controller Client

---
+++ src/controllers/clientController.js
@@ -0,0 +1,141 @@
+const Client = require("../models/client");
+const bcrypt = require("bcryptjs");
+
+exports.create = (req, res) => {
+    // Valider la requête
+    if (!req.body) {
+        res.status(400).send({
+            message: "Le contenu ne peut pas être vide !"
+        });
+    }
+
+    // Hacher le mot de passe
+    const hashedPassword = bcrypt.hashSync(req.body.motDePasse, 8);
+
+    // Créer un client
+    const client = new Client({
+        nom: req.body.nom,
+        prenom: req.body.prenom,
+        dateNaissance: req.body.dateNaissance,
+        adresse: req.body.adresse,
+        email: req.body.email,
+        motDePasse: bcrypt.hashSync(req.body.motDePasse, 8)
+    });
+
+    // Sauvegarder le client dans la base de données
+    Client.create(client, (err, data) => {
+        if (err)
+            res.status(500).send({
+                message: err.message || "Une erreur est survenue lors de la création du client."
+            });
+        else res.send(data);
+    });
+};
+
+exports.findAll = (req, res) => {
+    Client.getAll((err, data) => {
+        if (err)
+            res.status(500).send({
+                message: err.message || "Une erreur est survenue lors de la récupération du client."
+            });
+        else res.send(data);
+    });
+};
+
+exports.findOne = (req, res) => {
+    Client.findById(req.params.clientId, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Client non trouvé avec l'id ${req.params.clientId}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Erreur de récupération du client avec l'id " + req.params.clientId
+                });
+            }
+        } else res.send(data);
+    });
+};
+
+exports.update = (req, res) => {
+    // Valider la requête
+    if (!req.body) {
+        res.status(400).send({
+            message: "Le contenu ne peut pas être vide !"
+        });
+    }
+
+    Client.updateById(
+        req.params.clientId,
+        new Client(req.body),
+        (err, data) => {
+            if (err) {
+                if (err.kind === "not_found") {
+                    res.status(404).send({
+                        message: `Client non trouvé avec l'id ${req.params.clientId}.`
+                    });
+                } else {
+                    res.status(500).send({
+                        message: "Erreur mise à jour des informations du client avec l'id " + req.params.clientId
+                    });
+                }
+            } else res.send(data);
+        }
+    );
+};
+
+exports.delete = (req, res) => {
+    Client.remove(req.params.clientId, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Client non trouvé avec l'id ${req.params.clientId}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Impossible de supprimer le client dont l' id est : " + req.params.clientId
+                });
+            }
+        } else res.send({ message: `Le client  a été supprimé avec succès !` });
+    });
+};
+
+
+const jwt = require('jsonwebtoken');
+
+// Authentification d'un client
+exports.login = (req, res) => {
+    const email = req.body.email;
+    const motDePasse = req.body.motDePasse;
+
+    Client.findByEmail(email, (err, client) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                return res.status(404).send({ message: "Client non trouvé." });
+            } else {
+                return res.status(500).send({ message: "Erreur lors de la recherche du client." });
+            }
+        }
+
+        const passwordIsValid = bcrypt.compareSync(motDePasse, client.motDePasse);
+
+        if (!passwordIsValid) {
+            return res.status(401).send({
+                accessToken: null,
+                message: "Mot de passe invalide!"
+            });
+        }
+
+        // durrée du tocken 24 heures
+        const token = jwt.sign({ id: client.id }, process.env.SECRET, {
+            expiresIn: 86400 
+        });
+
+        res.status(200).send({
+            id: client.id,
+            email: client.email,
+            accessToken: token
+        });
+    });
+};
